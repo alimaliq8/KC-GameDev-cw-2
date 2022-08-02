7e1e656075b6985eca0512eabf23721ca417e5e6
alimaliq8 <110349639+alimaliq8@users.noreply.github.com>
2022-08-02 16:16:35 +0300

    cw2

---
+++ cw2/Assets/world 3rd best car/botata.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botata : MonoBehaviour
+{
+    string name ="marcoss";
+    string evil ="chuncky";
+    float heroHeight = 5.4f;
+    float evilHeight = 6.6f;
+    int heroage = 22;
+    int evilage = 35;
+
+
+
+
+
+
+    void Start()
+    { 
+        // the end is cool
+      print("there is a new hero called " + name + " and his age and height was " + heroHeight + heroage + " he is a super hero that have laser eyes and he think that he can stop any criminal");
+      print("but one day there was an evil made a plan and stoll all the bank and his name is " + evil + " he is the strongest vililan in the world and his age and height was" + evilHeight + evilage +"the hero didnt see him");
+        print("but he didnt notice that the camera was there so thay saw everything and tell"+ name + "and he find his secret room when he saw him he just wanna kill him ");
+        print("suddenly he find out that "+ evil + " has super powers too his power is to reflect anything so when he reflect the laser he reflect it to the bell and fell of on his head");
+        print("when " + name + "take the mask off he find out he is ....... a normal guy");
+       
+
+
+
+
+
+
+
+
+
+    }
+}
